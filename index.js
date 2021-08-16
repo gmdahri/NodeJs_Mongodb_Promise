f62c5c685240bd63683d4ee8e332e4bb58a4ab4a
gmdahri <gm24139@gmail.com>
2021-08-16 15:40:14 +0500

    mongoose update version of comments

--- index.js
+++ index.js
@@ -15,10 +15,24 @@ connect.then((db)=>{
     .then((dish)=>{
         console.log(dish);
 
-        return Dishes.find({}).exec();
+        return Dishes.findByIdAndUpdate(dish._id,{
+            $set:{description: 'updated test'},
+        },{
+            new: true
+        }).exec();
     })
-    .then((dishes)=>{
-        console.log(dishes);
+    .then((dish)=>{
+        console.log(dish);
+
+        dish.comments.push({
+            rating:5,
+            comment:'best',
+            author:'ahsan',
+        });
+        return dish.save();
+    })
+    .then((dish)=>{
+        console.log(dish)
         return Dishes.remove({});
     })
     .then(()=>{
--- models/dishes.js
+++ models/dishes.js
@@ -1,5 +1,23 @@
 const mongoose=require('mongoose');
 const Schema = mongoose.Schema;
+const commentSchema = new Schema({
+    rating:{
+        type: Number,
+        min:1,
+        max:5,
+        required: true
+    },
+    comment: {
+        type:String,
+        required: true
+    },
+    author:{
+        type: String,
+        required: true
+    },
+}, {
+        timestamps:true,
+});
 
 const dishSchema = new Schema({
  name:{
@@ -11,6 +29,7 @@ const dishSchema = new Schema({
      type: String,
      required: true
  },
+ comments:[commentSchema]
 },{
     timestamps: true
 });
